35072709b7528bcb8864996500ea563e44821ab5
Attila Kiss <attila.kiss@e-let.hu>
2014-02-05 18:41:58 +0100

    első

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,11 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/nl.x-services.plugins.socialsharing/www/SocialSharing.js",
+        "id": "nl.x-services.plugins.socialsharing.SocialSharing",
+        "clobbers": [
+            "window.plugins.socialsharing"
+        ]
+    }
+]
+});
